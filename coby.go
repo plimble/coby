cef3da7d257e7931c97b585db95ce0f1aea629a2
xier <xier.kokis@gmail.com>
2015-03-31 14:42:21 +0700

    fix create method param

--- coby.go
+++ coby.go
@@ -30,7 +30,7 @@ func NewService(store Store, expires time.Duration) *CobyService {
 	}
 }
 
-func (c *CobyService) Create(v map[string]interface{}) (*Token, error) {
+func (c *CobyService) Create(v interface{}) (*Token, error) {
 	t := &Token{
 		Token:  c.unik.Generate(),
 		Expire: c.moment.AddNowUnix(c.expires),
